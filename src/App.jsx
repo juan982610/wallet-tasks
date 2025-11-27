8551d2574351307cb1b39800476b46b4ad58ac67
Juan Posada <juand.posada10@gmail.com>
2025-11-26 21:36:58 -0500

    folder structure change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 ﻿import { Routes, Route } from "react-router-dom"
 
-import Sidebar from "./components/Sidebar"
-import Header from "./components/Header"
+import Sidebar from "./components/layout/Sidebar"
+import Header from "./components/layout/Header"
 
 // Páginas
 import Transactions from "./pages/Transactions"
